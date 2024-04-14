967fe2d13c69f8f918975101e03520ced644cb9b
wetracked1 <166954761+wetracked1@users.noreply.github.com>
2024-04-15 00:18:47 +0700

    Update and rename CNAME to CNAME.js
    Signed-off-by: wetracked1 <166954761+wetracked1@users.noreply.github.com>

---
+++ CNAME.js
@@ -0,0 +1,84 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Event",
+      "name": "wetracked1.com",
+      "startDate": "2024-07-21T19:00:00-05:00",
+      "endDate": "2024-07-21T23:00-05:00",
+      "eventStatus": "https://schema.org/EventScheduled",
+      "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
+      "location": {
+      "@type": "Location",
+      "url": "http://www.wetracked1.com"/>
+  },
+   </script> 
+ <script type="text/JavaScript">
+  function showpageview(auto) {
+       var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("script src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript"));               
+        Android sdk.showreview(auto);
+              var pageTracker = _gat._getTracker("G-xxxxx");
+          pageTracker._trackPageview();
+     }
+ </script>
+  <script type="text/javascript">
+ new PerformanceObserver((entryList) => {
+  const entries = entryList.getEntries();
+
+  for (const entry of entries) {
+    // Some resources might have a responseStart value of 0 if they're being
+    // cached, or if a cross-origin resource is served without a
+    // Timing-Allow-Origin header set.
+    if (entry.responseStart > 0) {
+      console.log(`TTFB: ${entry.responseStart}`, entry.name);
+    }
+  }
+}).observe({
+  type: 'resource',
+  buffered: true
+});
+observer.observe({ type: "navigation", buffered: true });
+</script>
+ <script type="text/javascript">
+ // server.mjs
+import { createServer } from 'node:http';
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+// starts a simple http server location port 8080
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+// run with `node server.mjs`
+</script>
+ <script type="application/ld+json">
+ {
+  "name": "wetracked1.com",
+  "author": "Evan You <wetracked9@gmail.com>",
+  "description": "Simple, Fast & Composable MVVM for building interactive interfaces",
+  "filename": "wetracked1.com",
+  "sri": null,
+  "version": "3.2.37",
+  "repository": {
+    "type": "CNAME",
+    "url": "http://www.wetracked1.com/mine.js",
+  },
+  "autoupdate": {
+    "source": "npm",
+    "target": "wetracked1.com",
+    "filepag": [
+      {
+        "basePath": "dist",
+        "files": [
+          "**/*.+(js|pag)"
+        ]
+      }
+    ]
+  }
+}
+</script>
+  
+  
+
+
